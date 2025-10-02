5a486ed3e073100df802361625fe88bb66add483
Johan Ek <johan.ek@tuta.com>
2025-10-03 01:36:10 +0200

    removed unused code

--- nixos/laptop/laptop.nix
+++ nixos/laptop/laptop.nix
@@ -19,16 +19,6 @@
     ];
   };
     
-  hardware = {
-    opengl = {
-      enable = true;
-      extraPackages = with pkgs; [
-        vaapiVdpau
-        libvdpau-va-gl
-      ];
-    };
-  };
-
   services.xserver.videoDrivers = [ "nvidia" ]; 
 
   hardware.nvidia = {
