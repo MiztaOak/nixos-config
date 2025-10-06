12137039c4a8474824f2508971ba1c08d91fca3e
Johan Ek <johan.ek@tuta.com>
2025-10-06 21:02:34 +0200

    WORKING HYPRLAND WORKSPACE ON MULTIPLE MONITORS (thanks random catgirl)

--- nixos/nixos-desktop/desktop.nix
+++ nixos/nixos-desktop/desktop.nix
@@ -10,10 +10,23 @@
     # ./hardware-configuration.nix
   ];
 
-  boot.initrd.kernelModules = [ "amdgpu" ];
+  # Amd graphics drivers
 
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+  boot.initrd.kernelModules = [ "amdgpu" ];
   services.xserver.enable = true;
   services.xserver.videoDrivers = [ "amdgpu" ];
+  hardware.graphics.extraPackages = with pkgs; [
+    rocmPackages.clr.icd
+  ];
+
+  environment.systemPackages = with pkgs; [
+    clinfo
+  ];
+
   fileSystems = {
     "/".options = [ "compress=zstd" ];
   };
