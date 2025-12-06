48f77322ed250bb84d1e9b602371d4cccd82a808
Johan Ek <johan.ek@tuta.com>
2025-12-06 23:57:54 +0100

    Made qutebrowser default browser

--- home-manager/qutebrowser.nix
+++ home-manager/qutebrowser.nix
@@ -35,4 +35,15 @@
     };
     extraConfig = lib.fileContents ./gruvbox.py;
   };
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "org.qutebrowser.qutebrowser.desktop";
+      "x-scheme-handler/http" = "org.qutebrowser.qutebrowser.desktop";
+      "x-scheme-handler/https" = "org.qutebrowser.qutebrowser.desktop";
+      "x-scheme-handler/about" = "org.qutebrowser.qutebrowser.desktop";
+      "x-scheme-handler/unknown" = "org.qutebrowser.qutebrowser.desktop";
+    };
+  };
 }
