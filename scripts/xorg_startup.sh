18c0d10761fc820f756a1e5c0beacf0c8caf188e
Johan Ek <johan.ek@tuta.com>
2026-01-28 21:35:04 +0100

    Updated nix to the version that does not break xorg... and did some small config stuff

--- scripts/xorg_startup.sh
+++ scripts/xorg_startup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
  
-xrandr --output DisplayPort-2 --primary --mode 1920x1080 --rate 144 --right-of HDMI-A-0 --output HDMI-A-0 --mode 1920x1080 --rate 75 &
+xrandr --output DP-3 --primary --mode 1920x1080 --rate 144 --right-of HDMI-1 --output HDMI-1 --mode 1920x1080 --rate 75 &
 xset s off -dpms
